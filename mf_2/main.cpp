53b852a576ca556319f5c687f4566580e5617b79
bloodfallen <alexejberdnikovpuska@yandex.ru>
2025-03-13 22:04:15 +0700

    trained makefiles

--- mf_2/main.cpp
+++ mf_2/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include "math.h"
 
-using namespace std;
-
 int main() {
     int a = 5;
     int b = 7;
     int result = add(a, b);
     
-    cout << "The sum of " << a << " and " << b << " is " << result << endl;
+    std::cout << "The sum of " << a << " and " << b << " is " << result << std::endl;
     return 0;
 }
